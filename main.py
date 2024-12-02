925b6feb1f760395af31c0f74b4ba52206ef71bd
Mike Peterson <mike@lemonshell.com>
2024-12-01 17:17:57 -0800

    follow link and scrape product page

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 from pprint import pprint
-from typing import Dict, List, Optional
+from typing import List, Optional
+from urllib.parse import parse_qs, urlencode, urlsplit, urlunsplit
 
-from bs4 import BeautifulSoup, Tag
 from pydantic import BaseModel
 
 from Page import Page
 
 
 class WineItem(BaseModel):
+    product_link: Optional[str] = None
     name: Optional[str] = None
+    notes: Optional[str] = None
     # sku: Optional[str] = None
-    # notes: Optional[str] = None
     # origin: Optional[str] = None
     # type_varietal: Optional[str] = None
     # alcohol_content: Optional[str] = None
     # price: Optional[str] = None
     # image: Optional[str] = None
-    product_link: Optional[str] = None
+
+    def __init__(self, base_url: str, path: str) -> None:
+        super().__init__()
+
+        url = base_url + path
+        self.product_link = url
+
+        soup = Page(url).soup
+        sections = soup.select("main>section")
+
+        if len(sections) == 3:
+            image = sections[0].select_one("img")
+            if image:
+                srcset = image.get_attribute_list("srcset")
+                print(srcset)
+
+            title = sections[0].select_one("h1")
+            if title:
+                self.name = title.text.strip()
+
+            note = sections[0].select_one(
+                'div:has(>svg[data-testid="FormatQuoteIcon"])'
+            )
+            if note:
+                self.notes = note.text.strip()
 
 
 class WineItems(BaseModel):
@@ -28,22 +53,33 @@ class WineItems(BaseModel):
         url = f"{base_url}/Products?searchText={search_text}"
         soup = Page(url).soup
 
-        products = soup.select(".tf-product-content")
+        products = soup.select(".tf-product-content>.tf-product-header>a")
 
         for i in range(min(len(products), limit)):
             product = products[i]
 
-            data = WineItem()
+            links = product.get_attribute_list("href")
+            if len(links) > 0:
+                split_url = urlsplit(str(links[0]))
 
-            header = product.select_one(".tf-product-header>a")
-            if header is not None:
-                data.name = header.text.strip()
-                links = header.get_attribute_list("href")
-                if len(links) > 0:
-                    path: str = str(links[0])
-                    data.product_link = base_url + path
+                query_params = parse_qs(split_url.query)
+                filtered_query_params = {
+                    key: query_params[key] for key in ["i"] if key in query_params
+                }
 
-        self.data.append(data)
+                new_query_string = urlencode(filtered_query_params, doseq=True)
+
+                path = urlunsplit(
+                    (
+                        split_url.scheme,
+                        split_url.netloc,
+                        split_url.path,
+                        new_query_string,
+                        split_url.fragment,
+                    )
+                )
+
+                self.data.append(WineItem(base_url, path))
 
 
 search_text = "vodka"
