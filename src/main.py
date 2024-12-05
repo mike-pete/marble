a83166e74b1fc7317283d62f3492f092ede24e66
Mike Peterson <mike@lemonshell.com>
2024-12-04 16:45:36 -0800

    add some prints

--- src/main.py
+++ src/main.py
@@ -1,14 +1,26 @@
 from pprint import pprint
 
-from example_wine_items import exact_match, mismatch
+from example_wine_items import exact_match
 from KLWines import KLWineItem, KLWines, compare_klwine_items
 
 search_text = "vodka"
-kl_wine_items = KLWines(search_text, limit=1).model_dump(by_alias=True)["data"]
+number_of_items_to_scrape = 10
 
+print(f"Scraping {number_of_items_to_scrape} \"{search_text}\" Items:\n\n")
+
+kl_wine_items = KLWines(search_text, limit=number_of_items_to_scrape).model_dump(
+    by_alias=True
+)["data"]
+
+pprint(kl_wine_items)
+
+
+
+print("\n\nComparing Each Scraped Item Against Comparison Item:")
+comparison_item = exact_match
 
 for wine_item in kl_wine_items:
     base_item = KLWineItem(**wine_item)
-    comparison_item = mismatch
     if comparison := compare_klwine_items(base_item, comparison_item):
+        print('\n')
         pprint(comparison.model_dump())
